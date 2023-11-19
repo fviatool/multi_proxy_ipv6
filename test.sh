23ef1b84e184d0f37c9a6fac199f848e9600f8e7
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-19 22:54:18 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,10 +6,12 @@ random() {
     echo
 }
 
-# Function to generate a random IPv6 address
+# Array for hexadecimal values
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
+# Function to generate an IPv6 address
 gen64() {
     ip64() {
-        array=("1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "a" "b" "c" "d" "e" "f")
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
     }
     echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
@@ -18,67 +20,61 @@ gen64() {
 # Function to install 3proxy
 install_3proxy() {
     echo "Installing 3proxy..."
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
+    yes | cp -f src/3proxy /usr/local/etc/3proxy/bin/
+    systemctl stop 3proxy
+    systemctl start 3proxy
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
     cd $WORKDIR
-    echo "3proxy installed successfully."
-}
 
 # Function to generate 3proxy configuration
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 2000
-nserver 1.1.1.1
-nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
+maxconn 1000
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6291456 
 flush
 auth strong
-
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\nproxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\nflush\n"}' ${WORKDATA})
 EOF
 }
 
-# Function to generate proxy file for user
-gen_proxy_file_for_user() {
-    awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA} > proxy.txt
+# Function to upload proxy details
+upload_proxy() {
+    local PASS=$(random)
+    echo "$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})" > proxy.txt
+    URL=$(curl -s --upload-file proxy.txt https://transfer.sh/proxy.txt)
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download proxy list from: ${URL}"
+    echo "Password: ${PASS}"
 }
 
-# Function to generate data
+# Function to generate data for proxy
 gen_data() {
-    userproxy=$(random)
-    passproxy=$(random)
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$userproxy/$passproxy/$IP4/$port/$(gen64 $IP6)"
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
 # Function to generate iptables rules
 gen_iptables() {
-    awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 " -m state --state NEW -j ACCEPT"}' ${WORKDATA}
+    cat <<EOF
+iptables -A INPUT -p tcp --dport 3128 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 1080 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -m state --state NEW -j ACCEPT
+$(awk -F "/" '{print "iptables -A INPUT -p tcp --dport " $4 " -s " $3 " -m state --state NEW -j ACCEPT"}' ${WORKDATA})
+EOF
 }
-
-# Function to generate ifconfig commands
-gen_ifconfig() {
-    awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA}
-}
-
-# Function to rotate proxies
 rotate_proxies() {
     while true; do
         sleep 600  # Sleep for 10 minutes
