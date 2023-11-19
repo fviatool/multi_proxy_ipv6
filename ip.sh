ea982578cf8f7a997fa07ea6779b6b2f289010e1
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-20 01:26:18 +0700

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#!/bin/bash
+
+CONFIG_FILE="/etc/app_config.conf"
+PROXY_CONFIG_FILE="/etc/3proxy/3proxy.cfg"
+LOG_FILE="/var/log/3proxy.log"
+
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
@@ -101,14 +107,140 @@ show_proxy_list() {
     cat proxy.txt
 }
 
-menu() {
-    clear
-    echo "Menu:"
-    echo "1. Create proxy and download"
-    echo "2. Rotate proxies"
-    echo "3. Show proxy list"
-    echo "4. Download proxy list"
-    echo "5. Exit"
+display_menu() {
+  clear
+  echo "========== 3Proxy Management Menu =========="
+  echo "[1] Enable IP Authentication"
+  echo "[2] Disable IP Authentication"
+  echo "[3] Generate New Ports"
+  echo "[4] Enable Auto Rotate"
+  echo "[5] Create and Download Proxies"
+  echo "[6] Show Proxy List"
+  echo "[7] Exit"
+  echo "============================================"
+}
+
+menu_option() {
+  read -p "Enter your choice [1-7]: " choice
+  case $choice in
+    1) enable_ip_authentication ;;
+    2) disable_ip_authentication ;;
+    3) generate_new_ports ;;
+    4) enable_auto_rotate ;;
+    5) create_and_download_proxies ;;
+    6) show_proxy_list ;;
+    7) exit ;;
+    *) echo "Invalid option. Please choose again." ;;
+  esac
+}
+
+apply_configuration_changes() {
+  # This is a placeholder function.
+  # In a real implementation, you might reload or apply your specific configurations here.
+  echo "Applying configuration changes..."
+  # Example: systemctl restart your_service
+  sleep 2
+}
+
+enable_ip_authentication() {
+  echo "Enabling IP Authentication..."
+
+  if [ -f "$CONFIG_FILE" ]; then
+    sed -i 's/IP_AUTHENTICATION=false/IP_AUTHENTICATION=true/' "$CONFIG_FILE"
+    apply_configuration_changes
+  else
+    echo "Error: Configuration file not found."
+  fi
+
+  echo "IP Authentication enabled successfully."
+  sleep 2
+}
+
+disable_ip_authentication() {
+  echo "Disabling IP Authentication..."
+
+  if [ -f "$CONFIG_FILE" ]; then
+    sed -i 's/IP_AUTHENTICATION=true/IP_AUTHENTICATION=false/' "$CONFIG_FILE"
+    apply_configuration_changes
+  else
+    echo "Error: Configuration file not found."
+  fi
+
+  echo "IP Authentication disabled successfully."
+  sleep 2
+}
+
+generate_new_ports() {
+  echo "Generating New Ports..."
+
+  starting_port=60000
+  number_of_ports=5
+
+  for ((i = 0; i < number_of_ports; i++)); do
+    new_port=$((starting_port + i))
+    echo "New Port: $new_port"
+    # Your logic to use the new port as needed
+  done
+
+  echo "New Ports generated successfully."
+  sleep 2
+}
+
+enable_auto_rotate() {
+  echo "Enabling Auto Rotate..."
+
+  auto_rotate=true
+
+  while [ "$auto_rotate" = true ]; do
+    rotate_proxies
+    sleep 600  # Sleep for 10 minutes
+  done
+
+  echo "Auto Rotate disabled."
+}
+
+create_and_download_proxies() {
+  echo "Creating and Downloading Proxies..."
+
+  gen_data > "$PROXY_CONFIG_FILE"
+  download_proxy
+  echo "Proxies created and downloaded successfully."
+  sleep 2
+}
+
+download_proxy() {
+  # Your logic for downloading proxies
+  # Example: curl -F "file=@$PROXY_CONFIG_FILE" https://transfer.sh
+  echo "Downloading proxies..."
+}
+
+show_proxy_list() {
+  echo "Proxy List:"
+  # Your logic to display the proxy list
+  # Example: cat proxy.txt
+}
+
+rotate_proxies() {
+  echo "Rotating proxies..."
+  new_ipv6=$(get_new_ipv6)
+  update_3proxy_config "$new_ipv6"
+  restart_3proxy
+  echo "Proxies rotated successfully."
+}
+
+get_new_ipv6() {
+  random_ipv6=$(openssl rand -hex 8 | sed 's/\(..\)/:\1/g; s/://1')
+  echo "$random_ipv6"
+}
+
+update_3proxy_config() {
+  new_ipv6=$1
+  sed -i "s/old_ipv6_address/$new_ipv6/" "$PROXY_CONFIG_FILE"
+}
+
+restart_3proxy() {
+  # Your logic to restart the 3proxy service
+  systemctl restart 3proxy.service
 }
 
 echo "Installing apps"
@@ -169,34 +301,8 @@ rotate_and_restart &
 
 chmod 0755 /etc/rc.local
 
+# Main menu loop
 while true; do
-    menu
-    read -p "Choose an option (1-5): " choice
-
-    case $choice in
-        1)
-            gen_data >$WORKDIR/data.txt
-            gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-            echo "Proxy created ."
-            download_proxy
-            ;;
-        2)
-            rotate_proxies
-            ;;
-        3)
-            show_proxy_list
-            ;;
-        4)
-            download_proxy
-            ;;
-        5)
-            echo "Exiting..."
-            exit 0
-            ;;
-        *)
-            echo "Invalid option. Please choose from 1 to 5."
-            ;;
-    esac
-
-    read -p "Press Enter to continue..."
+  display_menu
+  menu_option
 done
