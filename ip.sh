bf0c439fbce390f670168205e24dd32ef48b986c
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-24 21:07:11 +0700

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,17 +1,12 @@
-
- #!/bin/bash
-
-CONFIG_FILE="/etc/app_config.conf"
-PROXY_CONFIG_FILE="/usr/local/etc/3proxy/3proxy.cfg"
-LOG_FILE="/var/log/3proxy.log"
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
-array=("1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "a" "b" "c" "d" "e" "f")
-
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -20,19 +15,26 @@ gen64() {
 }
 
 install_3proxy() {
-    echo "Installing 3proxy"
+    echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6 || exit 1
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
-    cd $WORKDIR || exit 1
+    cd $WORKDIR
 }
 
-download_proxy() {
-    cd /home/cloudfly
-    curl -F "file=@proxy.txt" https://transfer.sh
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "//$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
 }
 
 gen_3proxy() {
@@ -49,36 +51,82 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth strong
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
+$(awk -F "/" '{print "\n" \
+"" $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
 
 gen_proxy_file_for_user() {
-    awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA} > proxy.txt
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
 }
 
-gen_data() {
-    userproxy=$(random)
-    passproxy=$(random)
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$userproxy/$passproxy/$IP4/$port/$(gen64 $IP6)"
-    done
+rotate_script="${WORKDIR}/rotate_proxies.sh"
+echo '#!/bin/bash' > "$rotate_script"
+echo 'new_ipv6=$(get_new_ipv6)' >> "$rotate_script"
+echo 'update_3proxy_config "$new_ipv6"' >> "$rotate_script"
+echo 'restart_3proxy' >> "$rotate_script"
+chmod +x "$rotate_script"
+
+# Add rotation to crontab for automatic rotation
+add_rotation_cronjob() {
+    echo "*/10 * * * * $rotate_script" >> /etc/crontab
 }
 
-gen_iptables() {
-    awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}
-}
+echo "Installing necessary packages..."
+yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
-gen_ifconfig() {
-    awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA}
-}
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
+EOF
+
+install_3proxy
+
+echo "Thư mục làm việc = /home/cloudfly"
+WORKDIR="/home/cloudfly"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir "$WORKDIR" && cd "$_"
+
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "IP nội bộ = ${IP4}. Subnet IPv6 ngoại trời = ${IP6}"
+
+while :; do
+    read -p "Nhập FIRST_PORT từ 10000 đến 60000: " FIRST_PORT
+    [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Nhập một số hợp lệ"; continue; }
+    if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
+        echo "OK! Số hợp lệ"
+        break
+    else
+        echo "Số nằm ngoài phạm vi, hãy thử lại"
+    fi
+done
+LAST_PORT=$(($FIRST_PORT + 2000))
+echo "LAST_PORT là $LAST_PORT. Tiếp tục..."
+
+gen_data > "${WORKDIR}/data.txt"
+gen_iptables > "${WORKDIR}/boot_iptables.sh"
+
+gen_3proxy > "/usr/local/etc/3proxy/3proxy.cfg"
+
+cat >> /etc/rc.local <<EOF
+bash ${WORKDIR}/boot_iptables.sh
+ulimit -n 10048
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
+EOF
+
+chmod 0755 /etc/rc.local
+bash /etc/rc.local
+
+gen_proxy_file_for_user
+rm -rf /root/3proxy-3proxy-0.8.6
+echo "Starting Proxy"
 
 rotate_proxies() {
     while true; do
