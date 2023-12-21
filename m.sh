982b83c5003675da716fc8438d0bd43379973abf
FVIA <130783224+fviatool@users.noreply.github.com>
2023-12-22 06:54:20 +0700

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,30 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
+
 install_3proxy() {
-    echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    echo "Installing 3proxy..."
+    URL="https://github.com/z3APA3A/3proxy/archive/0.8.6.tar.gz"
+    wget -qO- $URL | tar -xz
+    cd 3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
-    #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    #chmod +x /etc/init.d/3proxy
-    #chkconfig 3proxy on
-    cd $WORKDIR
+    cd ..
+    rm -rf 3proxy-0.8.6
 }
 
 gen_3proxy() {
@@ -49,170 +49,66 @@ $(awk -F "/" '{print "\n" \
 EOF
 }
 
-gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
-EOF
-}
-
-
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "//$IP4/$port/$(gen64 $IP6)"
+        echo "$(gen64 $IP6)"
     done
 }
 
 gen_iptables() {
-    cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
-EOF
+    awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}
 }
 
 gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
+    awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA}
 }
 
-cat << EOF > /etc/rc.d/rc.local
-#!/bin/bash
-touch /var/lock/subsys/local
-EOF
+WORKDIR="/root"
+WORKDATA="${WORKDIR}/ipv6.txt"
 
-echo "installing apps"
+echo "Installing required packages..."
 yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "working folder = /home/cloudfly"
-WORKDIR="/home/cloudfly"
-WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
-
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+echo "Internal IP: ${IP4}. External sub for IPv6: ${IP6}"
 
 while :; do
-  read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
-  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
-  if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
-    echo "OK! Valid number"
-    break
-  else
-    echo "Number out of range, try again"
-  fi
+    read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
+    [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
+    if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
+        echo "OK! Valid number"
+        break
+    else
+        echo "Number out of range, try again"
+    fi
 done
+
 LAST_PORT=$(($FIRST_PORT + 5555))
-echo "LAST_PORT is $LAST_PORT. Continue..."
+echo "LAST_PORT is $LAST_PORT. Continuing..."
 
-gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x boot_*.sh /etc/rc.local
+gen_data >${WORKDIR}/ipv6.txt
 
-gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-
-cat >>/etc/rc.local <<EOF
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
 
-bash /etc/rc.local
+chmod +x /etc/rc.d/rc.local
 
-gen_proxy_file_for_user
-rm -rf /root/3proxy-3proxy-0.8.6
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+gen_iptables >${WORKDIR}/boot_iptables.sh
+gen_ifconfig >${WORKDIR}/boot_ifconfig.sh
 
-echo "Starting Proxy"
+echo "Starting Proxy..."
 
 
 
-#!/bin/bash
 
-#!/bin/bash
-
-WORKDIR="/home/cloudfly"
-CONFIG_FILE="/usr/local/etc/3proxy/3proxy.cfg"
-
-update_3proxy_config() {
-    new_ipv6=$1
-    sed -i "s/old_ipv6_address/$new_ipv6/" $CONFIG_FILE
-}
-
-restart_3proxy() {
-    /usr/local/etc/3proxy/bin/3proxy $CONFIG_FILE
-}
-
-get_new_ipv6() {
-    random_ipv6=$(openssl rand -hex 8 | sed 's/\(..\)/:\1/g; s/://1')
-    echo "$random_ipv6"
-}
-
-rotate_ipv6() {
-    echo "Rotating IPv6 addresses..."
-    new_ipv6=$(get_new_ipv6)
-    update_3proxy_config "$new_ipv6"
-    restart_3proxy
-    echo "3proxy restarted successfully."
-    echo "IPv6 rotation completed."
-}
-
-create_proxy() {
-    echo "Creating a new proxy..."
-    # Add logic to create a new proxy here
-    echo "Proxy created successfully."
-}
-
-show_proxy_list() {
-    echo "Proxy list:"
-    # Add logic to show the proxy list here
-}
-
-show_menu() {
-    echo "----- MENU -----"
-    echo "1. Create Proxy"
-    echo "2. Rotate IPv6"
-    echo "3. Show Proxy List"
-    echo "4. Exit"
-    echo "-----------------"
-}
-
-handle_choice() {
-    read -p "Choose an option (1-4): " choice
-
-    case $choice in
-        1)
-            create_proxy
-            ;;
-        2)
-            rotate_ipv6
-            ;;
-        3)
-            show_proxy_list
-            ;;
-        4)
-            echo "Selected 4: Exit"
-            echo "Goodbye!"
-            exit 0
-            ;;
-        *)
-            echo "Invalid choice. Please choose from 1 to 4."
-            ;;
-    esac
-}
-
-main() {
-    while true; do
-        show_menu
-        handle_choice
-    done
-}
-
-# Auto rotate IPv6 every 10 minutes
-(crontab -l ; echo "*/10 * * * * $WORKDIR/rotate_proxies.sh") | crontab -
-
-# Start the main menu loop
-main
